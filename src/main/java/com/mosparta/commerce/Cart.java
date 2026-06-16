52d2d1682ad4c1a8a139da39247261fd6ffc5b82
momo <ptjkjm1@gmail.com>
2026-06-16 10:35:55 +0900

    feat(Cart): 장바구니 클래스 추가

---
+++ src/main/java/com/mosparta/commerce/Cart.java
@@ -0,0 +1,50 @@
+package com.mosparta.commerce;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Cart {
+    private final Map<Product, Integer> cartItems = new HashMap<>();
+
+    /**
+     * 장바구니 담기
+     */
+    public void addOneProduct(Product product) {
+        cartItems.merge(product, 1, Integer::sum);
+    }
+
+    public Integer getAllCount() {
+        return cartItems.values().stream().mapToInt(Integer::intValue).sum();
+    }
+
+    public Integer getTotalPrice() {
+        return cartItems.entrySet().stream()
+                .mapToInt(entry -> entry.getKey().getPrice() * entry.getValue())
+                .sum();
+    }
+
+    public boolean isAllInStock() {
+        return cartItems.entrySet().stream()
+                .allMatch(entry -> entry.getKey().isInStock(entry.getValue()));
+    }
+
+    public void buy() {
+        cartItems.forEach(Product::buy);
+        cartItems.clear();
+    }
+
+    public void clear() {
+        cartItems.clear();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[ 장바구니 내역 ]\n");
+        cartItems.forEach((key, val) -> {
+            sb.append(key.toSummaryString()).append(" | 수량: ").append(val).append("개\n");
+        });
+        sb.append("[ 총 주문 금액 ]\n").append(String.format("%,d", getTotalPrice()));
+
+        return sb.toString();
+    }
+}
