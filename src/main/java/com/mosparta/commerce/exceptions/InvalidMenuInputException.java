e3f87d5efb9fb7b61fdf0b4b49ededc57e18640c
momo <ptjkjm1@gmail.com>
2026-06-16 10:33:13 +0900

    feat(Exception): 커스텀 exception InvalidMenuInputException 생성

---
+++ src/main/java/com/mosparta/commerce/exceptions/InvalidMenuInputException.java
@@ -0,0 +1,8 @@
+package com.mosparta.commerce.exceptions;
+
+public class InvalidMenuInputException extends RuntimeException {
+
+    public InvalidMenuInputException(int input, int min, int max) {
+        super("'%d'는 유효하지 않은 입력입니다. %d ~ %d 사이의 숫자를 입력해주세요.".formatted(input, min, max));
+    }
+}
